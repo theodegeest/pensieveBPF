4aa1563f9efe4d4cf820120b593cbd8d74903b23
theodegeest <theo.degeest@gmail.com>
2026-03-08 13:36:46 +0100

    feat: profile block handling with offcpu
    Signed-off-by: theodegeest <theo.degeest@gmail.com>

--- src/pensieve.bpf.c
+++ src/pensieve.bpf.c
@@ -10,8 +10,8 @@
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
-// const volatile unsigned long long granularity_ns = 1e9;
-const volatile unsigned long long granularity_ns = 1e6;
+const volatile unsigned long long granularity_ns = 1e9;
+// const volatile unsigned long long granularity_ns = 1e6;
 
 struct internal_thread_info {
   u64 thread_creation_ts;
@@ -138,7 +138,7 @@ const volatile unsigned long long min_duration_ns = 0;
 //   return 0;
 // }
 
-static s64 get_block_index(u64 current_time, u64 start_time) {
+static u64 get_block_index(u64 current_time, u64 start_time) {
   if (current_time < start_time) {
     bpf_printk("get_block_index [%d] current is before start time\n");
     return -1;
@@ -169,6 +169,39 @@ static int create_new_thread_info(struct internal_thread_info *info_p,
   return bpf_map_update_elem(&thread_map, &pid, info_p, BPF_ANY);
 }
 
+static int submit_current_block(pid_t pid,
+                                struct internal_thread_info *info_p) {
+  struct profile_block *profile_block_p;
+
+  /* reserve sample from BPF ringbuf */
+  profile_block_p = bpf_ringbuf_reserve(&rb, sizeof(*profile_block_p), 0);
+  if (!profile_block_p)
+    return 0;
+
+  profile_block_p->pid = pid;
+  profile_block_p->block_index = info_p->block_index;
+  profile_block_p->block_start_time_ns = info_p->block_start_ts;
+  profile_block_p->first_event_time_ns = info_p->first_block_event_ts;
+  profile_block_p->last_event_time_ns = info_p->last_event_ts;
+  profile_block_p->offcpu_time_ns = info_p->offcpu_time_ns;
+  profile_block_p->end_state = info_p->state;
+
+  /* send data to user-space for post-processing */
+  bpf_ringbuf_submit(profile_block_p, 0);
+  return 0;
+}
+
+static int bump_block(struct internal_thread_info *info_p,
+                      u64 current_block_index, u64 current_time_ts) {
+  info_p->block_index = current_block_index;
+  info_p->block_start_ts =
+      start_of_block(current_time_ts, info_p->thread_creation_ts);
+  info_p->first_block_event_ts = current_time_ts;
+  info_p->last_event_ts = current_time_ts;
+  info_p->offcpu_time_ns = 0;
+  return 0;
+}
+
 // static struct internal_thread_info initial_thread_info = {};
 
 static int handle_sched_switch(void *ctx, bool preempt,
@@ -176,10 +209,10 @@ static int handle_sched_switch(void *ctx, bool preempt,
                                struct task_struct *next) {
   // struct internal_key *i_keyp, i_key;
   // struct val_t *valp, val;
-  s64 delta, current_block_index;
+  s64 delta;
   // u32 pid;
   pid_t pid, tgid;
-  u64 *scheduled_out_ts_p, scheduled_out_ts, current_time;
+  u64 current_time, current_block_index;
 
   struct internal_thread_info *info_p, info = {};
 
@@ -194,8 +227,8 @@ static int handle_sched_switch(void *ctx, bool preempt,
 
   // The scheduled out thread was not the idle thread
   if (pid) {
-    scheduled_out_ts = current_time;
-    bpf_map_update_elem(&scheduled_out_start, &pid, &scheduled_out_ts, 0);
+    // scheduled_out_ts = current_time;
+    // bpf_map_update_elem(&scheduled_out_start, &pid, &scheduled_out_ts, 0);
 
     info_p = bpf_map_lookup_elem(&thread_map, &pid);
     if (!info_p) {
@@ -228,7 +261,14 @@ static int handle_sched_switch(void *ctx, bool preempt,
     //            current_block_index);
 
     if (current_block_index > info_p->block_index) {
+      // The last event was in a previous block
+      // We need to submit this block to the user space
+      submit_current_block(pid, info_p);
+      bump_block(info_p, current_block_index, current_time);
     }
+
+    info_p->last_event_ts = current_time;
+    info_p->state = SCHEDULED_OUT;
   }
 
   // Handle the task that is just scheduled in
@@ -240,20 +280,74 @@ static int handle_sched_switch(void *ctx, bool preempt,
   if (!pid)
     return 0;
 
-  scheduled_out_ts_p = bpf_map_lookup_elem(&scheduled_out_start, &pid);
-  if (!scheduled_out_ts_p) {
-    // bpf_printk("handle_sched_switch [%d] pid (%d, %d) not found in "
-    //            "scheduled_out_start\n",
-    //            bpf_get_smp_processor_id(), pid, tgid);
-    return 0;
+  // scheduled_out_ts_p = bpf_map_lookup_elem(&scheduled_out_start, &pid);
+  // if (!scheduled_out_ts_p) {
+  //   // bpf_printk("handle_sched_switch [%d] pid (%d, %d) not found in "
+  //   //            "scheduled_out_start\n",
+  //   //            bpf_get_smp_processor_id(), pid, tgid);
+  //   return 0;
+  // }
+  info_p = bpf_map_lookup_elem(&thread_map, &pid);
+  if (!info_p) {
+    // This thread was not yet encountered
+    create_new_thread_info(&info, pid, SCHEDULED_OUT, current_time);
+    info_p = bpf_map_lookup_elem(&thread_map, &pid);
+    if (!info_p) {
+      bpf_printk(
+          "handle_sched_switch: Could not create thread info for next (%d)\n",
+          pid);
+      return 0;
+    }
   }
-  delta = (s64)(bpf_ktime_get_ns() - *scheduled_out_ts_p);
-  if (delta < 0)
-    goto cleanup;
 
   // bpf_printk(
   //     "handle_sched_switch [%d] pid (%d, %d) was scheduled out for %lld
   //     ns\n", bpf_get_smp_processor_id(), pid, tgid, delta);
+
+  current_block_index =
+      get_block_index(current_time, info_p->thread_creation_ts);
+
+  if (current_block_index > info_p->block_index) {
+    // The last event was in a previous block
+    // We need to submit this block to the user space
+    delta =
+        (s64)(info_p->block_start_ts + granularity_ns - info_p->last_event_ts);
+    if (delta < 0) {
+      bpf_printk(
+          "handle_sched_switch: next delta previous block negative (%d)\n",
+          pid);
+      goto cleanup;
+    }
+
+    if (delta > granularity_ns) {
+      bpf_printk("handle_sched_switch: WARNING (%d) delta of previous block is "
+                 "higher than granularity_ns\n",
+                 pid);
+    }
+
+    info_p->offcpu_time_ns += delta;
+    info_p->last_event_ts = info_p->block_start_ts + granularity_ns;
+    info_p->state = SCHEDULED_OUT;
+    submit_current_block(pid, info_p);
+    bump_block(info_p, current_block_index, current_time);
+  }
+
+  delta = (s64)(current_time - info_p->last_event_ts);
+  if (delta < 0) {
+    bpf_printk("handle_sched_switch: next delta current block negative (%d)\n",
+               pid);
+    goto cleanup;
+  }
+
+  if (delta > granularity_ns) {
+    bpf_printk("handle_sched_switch: WARNING (%d) delta of current block is "
+               "higher than granularity_ns\n",
+               pid);
+  }
+
+  info_p->last_event_ts = current_time;
+  info_p->offcpu_time_ns += delta;
+  info_p->state = SCHEDULED_IN;
 
   // delta /= 1000U;
   // valp = bpf_map_lookup_elem(&info, &i_keyp->key);
@@ -262,7 +356,7 @@ static int handle_sched_switch(void *ctx, bool preempt,
   // __sync_fetch_and_add(&valp->delta, delta);
 
 cleanup:
-  bpf_map_delete_elem(&scheduled_out_start, &pid);
+  // bpf_map_delete_elem(&scheduled_out_start, &pid);
   return 0;
 }
 
@@ -307,39 +401,6 @@ int trace_fork(struct trace_event_raw_sched_process_fork *ctx) {
     bpf_printk("fork (%d) failed to update element\n", pid);
   }
 
-  return 0;
-}
-
-static int submit_current_block(pid_t pid,
-                                struct internal_thread_info *info_p) {
-  struct profile_block *profile_block_p;
-
-  /* reserve sample from BPF ringbuf */
-  profile_block_p = bpf_ringbuf_reserve(&rb, sizeof(*profile_block_p), 0);
-  if (!profile_block_p)
-    return 0;
-
-  profile_block_p->pid = pid;
-  profile_block_p->block_index = info_p->block_index;
-  profile_block_p->block_start_time_ns = info_p->block_start_ts;
-  profile_block_p->first_event_time_ns = info_p->first_block_event_ts;
-  profile_block_p->last_event_time_ns = info_p->last_event_ts;
-  profile_block_p->offcpu_time_ns = info_p->offcpu_time_ns;
-  profile_block_p->end_state = info_p->state;
-
-  /* send data to user-space for post-processing */
-  bpf_ringbuf_submit(profile_block_p, 0);
-  return 0;
-}
-
-static int bump_block(struct internal_thread_info *info_p,
-                      u64 current_block_index, u64 current_time_ts) {
-  info_p->block_index = current_block_index;
-  info_p->block_start_ts =
-      start_of_block(current_time_ts, info_p->thread_creation_ts);
-  info_p->first_block_event_ts = current_time_ts;
-  info_p->last_event_ts = current_time_ts;
-  info_p->offcpu_time_ns = 0;
   return 0;
 }
 
@@ -393,6 +454,7 @@ int trace_exit(struct trace_event_raw_sched_process_template *ctx) {
   //            pid, info_p->thread_creation_ts, delta, current_block_index,
   //            start_of_block(current_time_ts, info_p->thread_creation_ts));
 
+  info_p->last_event_ts = current_time_ts;
   info_p->state = THREAD_EXIT;
   submit_current_block(pid, info_p);
   /* reserve sample from BPF ringbuf */
